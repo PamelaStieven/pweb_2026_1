9909e7e426ae006e7870015dd003a729a4577003
PamelaStieven <stieven.pamela13@gmail.com>
2026-04-27 17:18:42 -0300

    .

--- php/aula01.php
+++ php/aula01.php
@@ -7,16 +7,63 @@
 </head>
 <body>
     <?php
+
+    // olá mundo
         echo "Olá mundo PHP!";
+
+    //nome e idade
+        echo"<br>";
         $nome = "Pamela";
         $idade = 18;
-        echo "Meu nome é $nome e tenho $idade anos";
+        echo "<br> Meu nome é $nome e tenho $idade anos";
 
+    //mostrar se é maior ou menor de idade
         if ($idade < 18){
-            echo "Essa pessoa é menor de idade";
+            echo "<br> Sou de menor de idade";
         }else{
-            echo "Essa pessoa é maior de idade";
+            echo "<br> Sou de maior de idade";
         }
-    ?>
+
+    //notas
+        echo"<br>";
+        $notas =[5,7,10,9];
+        echo"<br>";
+        echo "As notas do aluno são:";
+        for ($i = 0; $i < count($notas); $i++){
+            echo $notas[$i]. " - ";
+        }
+        echo "<br>";
+        
+        foreach($notas as $item){
+            echo "A nota do aluno é:";
+            echo $item. "<br>";
+        }
+
+    //nomes
+         $nomes =["Pamela","Vicenzo","Paola","Kadson","Helena"];
+        echo"<br>";
+        echo "Tabela de nomes: <br>";
+        for ($i = 0; $i < count($nomes); $i++){
+            echo $nomes[$i]. " <br> ";
+        }
+        
+        foreach($nomes as $item){
+            echo "O nome é:";
+            echo $item. "<br>";
+        }
+        
+        //carros
+        echo"<br>";
+        $carro = ['modelo' => "Mustang", 'cor' => "Branco", 'ano' => 2026];
+
+        echo "O carro é: <br>";
+        echo $carro['modelo']. " - ". $carro['cor'];
+        
+        ?>
+        <p> Meu site <?= $carro['modelo']." - ano:" .$carro['ano'] ?> </p>
+        
+        <?php
+            include "./aula02.php";
+        ?>
 </body>
 </html>
