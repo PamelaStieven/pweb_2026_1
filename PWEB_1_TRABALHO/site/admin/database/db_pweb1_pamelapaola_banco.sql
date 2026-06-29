8245e968186ed2326683a0b0ffb7a647a0b1a572
PamelaStieven <stieven.pamela13@gmail.com>
2026-06-29 16:53:17 -0300

    Update db_pweb1_pamelapaola_banco.sql

--- PWEB_1_TRABALHO/site/admin/database/db_pweb1_pamelapaola_banco.sql
+++ PWEB_1_TRABALHO/site/admin/database/db_pweb1_pamelapaola_banco.sql
@@ -20,8 +20,7 @@ CREATE TABLE IF NOT EXISTS livros (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(150) NOT NULL,
     autor VARCHAR(100) NOT NULL,
-    genero VARCHAR(100) NOT NULL,
-    ano_publicacao INT NOT NULL
+    genero VARCHAR(100) NOT NULL
 );
 
 
